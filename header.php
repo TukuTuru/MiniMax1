6bad3cb462b0c90285633a4d3345f0cb8d16ab20
tukuturu <arbeit@yoonect.com>
2016-07-21 14:44:57 +0800

    wp_footer(), wp_head() and Theme Defaults
    Add calls to wp_footer(), wp_head(), add theme supports such as menu,
    title tag, and translation files and WooCommerce Support

--- header.php
+++ header.php
@@ -21,5 +21,8 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <!-- Always have wp_head() just before the closing </head> tag of your theme, or you will break many plugins, which
+         generally use this hook to add elements to <head> such as styles, scripts, and meta tags.-->
+    <?php wp_head();?>
   </head>
   <body <?php body_class(); ?>>
