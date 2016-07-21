2224cdcf15e4f20766928cae636546debcc5b9b1
tukuturu <arbeit@yoonect.com>
2016-07-21 11:32:19 +0800

    Enqueue Styles
    Enqueue the styles and scripts on functions.php

--- functions.php
+++ functions.php
@@ -6,3 +6,26 @@
  *
  * @since MiniMax1 1.0
  */
+
+/**
+ * Enqueue scripts and styles
+ */
+function minimax1_styles() {
+	//Enqueue the MiniMax1 Main Style Sheet
+	wp_enqueue_style( 'style', get_stylesheet_uri() );
+        
+	//Enqueue Bootstrap CSS
+	wp_enqueue_style( 'minimax1-bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css', '3.3.6', 'all' );
+        
+	/*
+	 * @todo Enqueue FontAwesome
+     *
+     */
+	//wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/css/font-awesome.min.css', array(), '4.2.0', 'all' );
+    }
+add_action( 'wp_enqueue_scripts', 'minimax1_styles' );
+
+function minimax1_scripts() {
+	//Enqueue Bootstrap JS
+	wp_enqueue_script('minimax1-bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', '3.3.6', true);
+}
