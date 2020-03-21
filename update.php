93b288170a8f5491d4c5765a1ede1c0854681313
Beda Schmid <arbeit@yoonect.com>
2020-03-22 00:48:08 +0700

    prefix

--- update.php
+++ update.php
@@ -8,46 +8,46 @@
  */
 
 //Pull updates from 
-$api_url = 'https://updates.tukutoi.com';
+$minimax1_api_url = 'https://updates.tukutoi.com';
 
 /**
  * Define Theme Data
  */
-$theme_data = wp_get_theme();
-$theme_version = $theme_data->get( 'Version' );    
-$theme_base = 'MinimaX1';
+$minimax1_theme_data = wp_get_theme();
+$minimax1_theme_version = $minimax1_theme_data->get( 'Version' );    
+$minimax1_theme_base = 'MinimaX1';
 
 /**
  * Check if updates are available
- * @param  [type] $checked_data [description]
- * @return [type] $checked_data [description]
+ * @param  [type] $minimax1_checked_data [description]
+ * @return [type] $minimax1_checked_data [description]
  */
-function check_for_update($checked_data) {
-	global $wp_version, $theme_version, $theme_base, $api_url;
+function minimax1_check_for_update($minimax1_checked_data) {
+	global $wp_version, $minimax1_theme_version, $minimax1_theme_base, $minimax1_api_url;
 
-	$request = array(
-		'slug' => $theme_base,
-		'version' => $theme_version 
+	$minimax1_request = array(
+		'slug' => $minimax1_theme_base,
+		'version' => $minimax1_theme_version 
 	);
 	// Start checking for an update
-	$send_for_check = array(
+	$minimax1_send_for_check = array(
 		'body' => array(
 			'action' => 'theme_update', 
-			'request' => serialize($request),
+			'request' => serialize($minimax1_request),
 			'api-key' => md5(get_bloginfo('url'))
 		),
 		'user-agent' => 'WordPress/' . $wp_version . '; ' . get_bloginfo('url')
 	);
-	$raw_response = wp_remote_post($api_url, $send_for_check);
-	if (!is_wp_error($raw_response) && ($raw_response['response']['code'] == 200))
-		$response = unserialize($raw_response['body']);
+	$minimax1_raw_response = wp_remote_post($minimax1_api_url, $minimax1_send_for_check);
+	if (!is_wp_error($minimax1_raw_response) && ($minimax1_raw_response['response']['code'] == 200))
+		$minimax1_response = unserialize($minimax1_raw_response['body']);
 
 	// Feed the update data into WP updater
-	if (!empty($response)) 
-		$checked_data->response[$theme_base] = $response;
+	if (!empty($minimax1_response)) 
+		$minimax1_checked_data->minimax1_response[$minimax1_theme_base] = $minimax1_response;
 
-	return $checked_data;
+	return $minimax1_checked_data;
 }
 
 //Hook to site_transient_update_themes
-add_filter('pre_set_site_transient_update_themes', 'check_for_update');
+add_filter('pre_set_site_transient_update_themes', 'minimax1_check_for_update');
